6d338030a93fbcc1e4327076a07630f6a20f5a69
Timo Heiten <timo.heiten@kpark.de>
2021-04-20 13:11:17 +0200

    Unit tests rqreply multiple bind on same socket added, manual tests refactored

---
+++ src/heitech.zer0mqXt.core/Infrastructure/Unit.cs
@@ -0,0 +1,7 @@
+namespace heitech.zer0mqXt.core.infrastructure
+{
+    ///<summary>
+    /// Response type for XtResult when none is needed
+    ///</summary>    
+    public class Unit { public static Unit Result() => new Unit(); private Unit() {} }
+}
--- src/heitech.zer0mqXt.core/Main/Socket.cs
+++ src/heitech.zer0mqXt.core/Main/Socket.cs
@@ -6,11 +6,11 @@ using heitech.zer0mqXt.core.patterns;
 
 namespace heitech.zer0mqXt.core.Main
 {
-    internal class Bus : IEntry
+    internal class Socket : ISocket
     {
         private readonly RqRep _rqRep;
         private readonly PubSub _pubSub;
-        public Bus(SocketConfiguration config) 
+        internal Socket(SocketConfiguration config) 
         {
             _rqRep = new RqRep(config);
             _pubSub = new PubSub(config);
@@ -59,14 +59,18 @@ namespace heitech.zer0mqXt.core.Main
             where TRequest : class, new()
             where TResult : class, new()
         {
-            _rqRep.Respond<TRequest, TResult>(callback, cancellationToken);
+            var result = _rqRep.Respond<TRequest, TResult>(callback, cancellationToken);
+            if (result.IsSuccess == false)
+                throw result.Exception;
         }
 
         public void RespondAsync<TRequest, TResult>(Func<TRequest, Task<TResult>> callback, CancellationToken cancellationToken = default)
             where TRequest : class, new()
             where TResult : class, new()
         {
-            // todo
+            var result = _rqRep.RespondAsync<TRequest, TResult>(callback, cancellationToken);
+            if (result.IsSuccess == false)
+                throw result.Exception;
         }
     }
 }
